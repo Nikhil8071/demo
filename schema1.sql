a2cd7f1c7881b1f0101bcac95a456df9270fddf2
Nikhil Kumar <nikhil4917305@gmail.com>
2024-12-28 10:54:15 +0530

    added insert command

--- schema1.sql
+++ schema1.sql
@@ -53,5 +53,16 @@ create table membership
 	total_paid			decimal
 );
 
+insert into bike values(1,	'Mountain Bike 1'	,'mountain bike'	,10.00	,50.00	,'available');
+insert into bike values(2,	'Road Bike 1'	,'road bike',12.00	,60.00	,'available');
+insert into bike values(3,	'Hybrid Bike 1'	,'hybrid'	,8.00	,40.00	,'rented');
+insert into bike values(4,	'Electric Bike 1'	,'electric',15.00	,75.00	,'available');
+insert into bike values(5,	'Mountain Bike 2'	,'mountain bike'	,10.00	,50.00	,'out of service');
+insert into bike values(6,	'Road Bike 2'	,'road bike',12.00	,60.00	,'available');
+insert into bike values(7,	'Hybrid Bike 2'	,'hybrid'	,8.00	,40.00	,'out of service');
+insert into bike values(8,	'Electric Bike 2'	,'electric',15.00	,75.00	,'available');
+insert into bike values(9,	'Mountain Bike 3'	,'mountain bike'	,10.00	,50.00	,'rented');
+insert into bike values(10,	'Road Bike 3'	,'road bike',12.00	,60.00	,'available');
+
 
 
